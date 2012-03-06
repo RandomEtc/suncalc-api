90138141675e13378706ff9dc45e7e07519554a5
Tom Carden <tom@tom-carden.co.uk>
2012-03-06 12:13:05 -0800

    add homepage

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ function requireFloat(name) {
     }
 }
 
+app.get('/', function(req,res){
+    var date = new Date().toISOString(),
+        home = process.env.HOME_URL || 'https://github.com/RandomEtc/suncalc-api';
+    res.send('<!DOCTYPE html><meta charset="utf-8"><body>' +
+        'I\'m <a href="https://github.com/mourner/suncalc">suncalc</a> over HTTP. ' +
+        'e.g. <a href="/times?lat=51.5&lon=-0.5&date='+date+'">/times</a> ' +
+        'or <a href="/position?lat=51.5&lon=-0.5&date='+date+'">/position</a>. ' +
+        'Date is optional and defaults to now. ' +
+        'I run on a single free Heroku dyno. ' +
+        '<a href="'+home+'">Issues/bugfixes welcome.</a>');
+})
+
 app.get('/times', requireFloat('lat'), requireFloat('lon'), function(req,res){
     var lat = parseFloat(req.query.lat),
         lon = parseFloat(req.query.lon),
